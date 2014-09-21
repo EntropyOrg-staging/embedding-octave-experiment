c17cadfba4ffc43717d33847e111d2b8fb065be7
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-09-21 16:01:12 -0500

    chomp output from octave-config

--- lib/OctaveInline.pm
+++ lib/OctaveInline.pm
@@ -2,11 +2,13 @@ package OctaveInline;
 
 use strict;
 use warnings;
+use File::Spec;
 
 sub Inline {
 	return unless $_[-1] eq 'CPP';
+	chomp(my $octave_dir = `octave-config -p OCTINCLUDEDIR`);
 	my $oct_inc = join " ", map { "-I$_" } (
-		`octave-config -p OCTINCLUDEDIR`,
+		$octave_dir,
 		"/usr/include/hdf5/serial",
 		);
 	my $oct_libs  = join(' ', "-L".`octave-config -p LIBDIR`, '-loctinterp');
